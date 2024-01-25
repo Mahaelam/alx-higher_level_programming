fe603c389f2ae266ed1078e441b9e2ab74982fcd
Mahaelam <elamraoui.m56@gmail.com>
2024-01-24 18:56:11 -0800

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range (10) :
-    for j in range (i, 10) :
-        if i < j:
-            print ("{:d) { :d)". format (i, i),
-                    end="\n" if i == 8 and j == 9 else ", ")
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def print_last_digit (number) :
-    number = abs (number) % 10
-    print(number, end="")
-    return number 
+def print_last_digit(number):
+    if number < 0:
+        last_digit = number % -(10)
+        print(-(last_digit), end='')
+    else:
+        last_digit = number % 10
+        print(last_digit, end='')
+    return abs(last_digit) 
